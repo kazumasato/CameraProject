67fc27d2368847f3383b347f2b0c384e0b171708
佐藤一馬 <>
2019-11-19 14:22:14 +0800

    レイアウトできた

--- CameraProject/ViewController.swift
+++ CameraProject/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
     }
-
-
+    //カメラの起動ボタン
+    @IBAction func runCamera(_ sender: UIButton) {
+    }
+    //アルバムボタン
+    @IBAction func runAlbum(_ sender: UIButton) {
+    }
+    //保存ボタン
+    @IBAction func savePhoto(_ sender: UIButton) {
+    }
+    
 }
 
